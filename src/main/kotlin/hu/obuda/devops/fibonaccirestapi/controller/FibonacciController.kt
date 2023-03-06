063123d6aed778669ec60ce5447674b543be414d
csonor <csdoba2@gmail.com>
2023-03-06 12:07:26 +0100

    added fibonacci logic

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
@@ -2,21 +2,25 @@ package hu.obuda.devops.fibonaccirestapi.controller
 
 import hu.obuda.devops.fibonaccirestapi.service.FibonacciService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/")
 class FibonacciController {
 
-    @Autowired
-    var fibonacciService: FibonacciService? = null
+    @Autowired var fibonacciService: FibonacciService? = null
 
     @GetMapping(value = ["fibonacci"])
     open fun fibonacci(@RequestParam n: Int): Int? {
-        // TODO - If n is greater than 46 then return BAD REQUEST use HttpStatus
-        return fibonacciService?.fibonacci(n)
+        return if (n > 46 || n < 0) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+        } else {
+            fibonacciService?.fibonacci(n)
+        }
     }
 }
--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Service
 @Service
 class FibonacciService {
     fun fibonacci(n: Int): Int {
-        return if (n == 1) 0
-        else 0 // TODO instead of this logic implement fibonacci
+        val fib = IntArray(n + 1)
+        fib[0] = 0
+        fib[1] = 1
+        for (i in 2..n) {
+            fib[i] = fib[i - 1] + fib[i - 2]
+        }
+        return fib[n]
     }
 }
